5573b06d771ee1784126caa554ff0b0528b6335c
Priscilla Polanco <polancop20@gmail.com>
2016-03-24 13:56:15 -0400

    completed midterm

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,18 +1,39 @@
-def new
-@article = Article.new
-end
-
-def create
-  @article = Article.new(article_params)
-
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  def index
+    @articles = Article.all
   end
-end
 
-private
-def article_params
-  params.require(:article).permit(:title, :text)
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+    def show
+      @article = Article.find(params[:id])
+    end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
